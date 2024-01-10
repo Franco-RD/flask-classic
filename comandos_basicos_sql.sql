5fa4e19e38e3fa0e92117312f545ec86047d5093
Franco-RD <franco.romano.data@gmail.com>
2024-01-10 16:03:06 -0300

    Ultimos cambios

---
+++ comandos_basicos_sql.sql
@@ -0,0 +1,20 @@
+/**/
+/* Seleciconar todo de una tabla*/
+/*SELECT * FROM movements*/
+
+/* Seleccionar solo unos campos*/
+/*SELECT concept,quantity FROM movements*/
+
+/* Insertar nuevos registros en la tabla*/
+/*INSERT INTO movements (date, concept, quantity) VALUES ("2023-12-10", "comida", -500);*/
+
+/* Seleccionar segun una condicion*/
+/*SELECT date,concept,quantity FROM movements WHERE quantity=2;*/
+
+/* Actualizar campo de la tabla (referenciar siempre por WHERE o actualiza toda la tabla) */
+/*UPDATE movements SET concept = "desayuno", quantity= -50 WHERE id=2;*/
+
+/* Borrar segun una condicion (si no se pone borra todo) */
+/* DELETE FROM movements WHERE id=3;*/
+
+
--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -3,4 +3,8 @@ from flask import render_template, request, redirect
 
 @app.route("/")
 def hello():
-    return "Hola esto es flask"
+    diccionario = [{"fecha":"2024-01-01", "concept":"Nomina Enero", "monto":1500},
+                   {"fecha":"2024-01-02", "concept":"Compra de reyes", "monto":-100},
+                   {"fecha":"2024-01-03", "concept":"Compra del mercadona", "monto":-100},
+                   {"fecha":"2024-01-04", "concept":"Compra de roscon", "monto":-50}]
+    return render_template("Index.html", variable = diccionario)
