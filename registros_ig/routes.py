0069ebef1d5c4d7e7f18bd3115fe59c190228af0
Franco-RD <franco.romano.data@gmail.com>
2024-01-11 18:10:04 -0300

    Hecha la vista de registro nuevo, preparando para hacer la funcionalidad de insertar nuevos registros a la base

--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -1,13 +1,38 @@
 from registros_ig import app
 from flask import render_template, request, redirect  
 from registros_ig.models import *
+from datetime import date
+
+def validarFormulario(datosFormulario):
+    errores = []
+    hoy = str(date.today())  #Fecha del dia para validacion. No se tiene que poder agregar gastos de dias mayores a hoy
+
+    if datosFormulario['date'] > hoy or datosFormulario['date'] == "":
+        errores.append("La fecha no puede ser mayor a la actual o vacia")    
+    if datosFormulario['concept'] == "":
+        errores.append("El concepto no puede ir vacio")
+    if datosFormulario['quantity'] == "" or float(datosFormulario['quantity']) == 0.0:  #El monto tiene que ir con int porque por defecto las entradas al formulario son str
+        errores.append("El monto debe ser distinto de cero y de vacio")
+    
+    return errores
+
+
+
 
 @app.route("/")
 def index():
     dic = select_all()
-    return render_template("Index.html", datos = dic)
+    return render_template("index.html", datos = dic)
 
-@app.route("/new")
+@app.route("/new", methods=["GET", "POST"])
 def create():
-
-    return "esto es una vista de registro"
+    if request.method == "GET":
+        return render_template("create.html", dataForm = {})
+    
+    else: 
+        errores = validarFormulario(request.form)
+        if errores:
+            return render_template("create.html", errors = errores, dataForm = request.form)
+        
+        return f"Enviar los datos a la base {request.form}"
+    
